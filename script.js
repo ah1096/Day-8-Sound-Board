5952777ae422e5db39e504e58e23de2faab03ec7
ah1096 <alyssa.holbert@student.awesomeinc.org>
2023-01-11 13:18:43 +0000

    changed files names, styled buttons + background

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['empire', 'mudcrabs', 'scum', 'stop', 'what', 'why']
 
 sounds.forEach(sound =>{
     const btn = document.createElement('button')
